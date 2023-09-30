07d89ec1328aa0fc2d3c7b62d612120fa053aa9d
Daniel Kinyua <my.github.kinyuad29@gmail.com>
2023-09-30 06:45:56 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoadedDOMContentLoaded", (event) => {
+    const myText = "JavaScript is so cool. It lets me add text to my page programmatically."; 
+ document.getElementById('Javascript is so cool. It lets me add text to my page programmatically').innerHTML = 'This is really cool';
+});
